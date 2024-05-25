b6f123869beaea9194e9e40faf19ca4a0ba5fe52
Albert <albert@buenaventura.cc>
2024-05-25 22:26:29 +0200

    Finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://pirimpompito-astro.netlify.app"
+  site: "https://pirimpompito-astro.netlify.app",
+  integrations: [preact()]
 });
